d3735c8a22488523fc2c74f11430ed7287c61962
2dam <you@example.com>
2022-09-20 14:21:48 +0200

    last one from class. only graphic part and buttons on MainActivity

--- app/src/main/java/com/example/pmd_reto2/MainActivity.java
+++ app/src/main/java/com/example/pmd_reto2/MainActivity.java
@@ -3,8 +3,21 @@ package com.example.pmd_reto2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button
+        button1 = null, button2 = null,
+        button3 = null, button4 = null,
+        button5 = null, button6 = null,
+        button7 = null, button8 = null,
+        button9 = null, button0 = null,
+        buttonPlus = null, buttonMinus = null,
+        buttonEquals = null;
+
+    private TextView text = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
